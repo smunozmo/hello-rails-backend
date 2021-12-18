d7bcb3864dddceab38d69efb3e94b6e4de868c06
Sebastián M <smunozmo@gmail.com>
2021-12-17 22:01:12 -0500

    Add seeds to database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(body: 'Hello World!')
+Message.create(body: 'Hola Mundo!')
+Message.create(body: 'Welcome buddy!')
+Message.create(body: 'Hi there!')
+Message.create(body: 'Howdy!')
